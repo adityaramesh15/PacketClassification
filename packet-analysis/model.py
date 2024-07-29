9b16fbcc5297593e49b65a682a1667c23afeebe2
Aditya Ramesh <adityaramesh15@gmail.com>
2024-07-28 19:50:50 -0500

    Added Location to DataFrames

--- packet-analysis/model.py
+++ packet-analysis/model.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import numpy as np
-import sklearn as sk
+from location import get_location
+from sklearn.ensemble import IsolationForest
+
+
+dpi_data = pd.read_json("../data/dpi-data.json")
+home_data = pd.read_json("../data/home-data.json")
+cafe_data = pd.read_json("../data/cafe-data.json")
+
+dpi_data['location'] = dpi_data.apply(lambda row: get_location(row['src_ip'], row['dst_ip']), axis = 1)
+home_data['location'] = home_data.apply(lambda row: get_location(row['src_ip'], row['dst_ip']), axis = 1)
+cafe_data['location'] = cafe_data.apply(lambda row: get_location(row['src_ip'], row['dst_ip']), axis = 1)
 
