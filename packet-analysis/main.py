4a5ccae5254b29791ded5f0a6016b0864a78cf64
Aditya Ramesh <adityaramesh15@gmail.com>
2024-07-30 15:55:56 -0500

    Collected more Data for training

--- packet-analysis/main.py
+++ packet-analysis/main.py
@@ -19,7 +19,7 @@ detector.load_model("anomaly_detector.pkl")
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 while True:
     raw_packet = r.blpop('packet_list', 0)[1]
-    if raw_packet != '{}':
+    if raw_packet != '':
         json_packet = json.loads(raw_packet)
         data = pd.DataFrame([json_packet])
         print(detector.predict(data))
